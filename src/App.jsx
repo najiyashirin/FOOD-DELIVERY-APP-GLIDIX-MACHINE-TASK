bfb8cc44d6f84766e02192e5bb910f8f9194d5c3
najiyashirin <najiya3012@gmail.com>
2025-11-08 23:29:13 +0530

    Removed unused route imports from App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+
 import "./App.css";
 
 const App = () => {
@@ -15,11 +11,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+     
       </Routes>
     </>
   );
