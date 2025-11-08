2d68ca4a28775b032a512898a20919210e02635b
najiyashirin <najiya3012@gmail.com>
2025-11-08 23:36:34 +0530

    sign form

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import foods from "../data/foods.json";
+
 
 const Menu = () => {
   return (
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+
+
+  return (
+    <div className="page">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSignup} className="auth-form">
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+
+
+export default Signup;
