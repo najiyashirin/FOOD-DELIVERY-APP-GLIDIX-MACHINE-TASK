a0571a54a01531af922bd9ae2fcddbe5b7c5adb2
najiyashirin <najiya3012@gmail.com>
2025-11-08 23:20:10 +0530

    Added Home component with welcome message

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div className="page">
+    <h1>Welcome to Eato</h1>
+    <p>Delicious meals delivered to your doorstep.</p>
+  </div>
+);
+
+export default Home;
