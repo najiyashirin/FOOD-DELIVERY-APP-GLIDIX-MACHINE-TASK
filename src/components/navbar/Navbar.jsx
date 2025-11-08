40123880b1040555c9383cc34f7be9b741ab0e92
najiyashirin <najiya3012@gmail.com>
2025-11-08 23:18:59 +0530

    created navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">Eato</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
