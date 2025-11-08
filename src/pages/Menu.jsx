fcb1fddc7547e4541d804e81106cdf4e5af9b163
najiyashirin <najiya3012@gmail.com>
2025-11-08 23:36:16 +0530

    Added signup form

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import foods from "../data/foods.json";
+
+const Menu = () => {
+  return (
+    <div className="page">
+      <h1>Our Menu</h1>
+      
+    </div>
+  );
+};
+
+export default Menu;
