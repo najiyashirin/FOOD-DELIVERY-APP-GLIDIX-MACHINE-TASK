fa649309c5ea9a376cfb9a77858bb924b7fb7a1a
najiyashirin <najiya3012@gmail.com>
2025-11-09 15:39:52 +0530

    Vite config: Set base path to resolve 404 on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 🎯 Add the 'base' property here:
+  base: "/FOOD-DELIVERY-APP-GLIDIX-MACHINE-TASK/", 
   plugins: [react()],
 })
